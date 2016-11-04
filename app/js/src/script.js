78f45a2554fa6cb06464cf997ae7544a45d30d4a
Andrei Dmytruk <mallboroks@mail.ru>
2016-11-05 01:53:03 +0200

    19

--- app/js/src/script.js
+++ app/js/src/script.js
@@ -1,3 +1,17 @@
 $(function () {
-  
-})
+  /*$('#panel1').click(function () {
+    if ($('',this).css("display") == "none") {
+      $("ul.accordion li ul").slideUp(500);
+      $("ul",this).slideDown(500);
+    }
+    return false;
+    $('#text1').css('display', 'block')
+  });*/
+
+  $("#accordion section h1").click(function(e) {
+  $(this).parents().siblings("section").addClass("ac_hidden");
+  $(this).parents("section").removeClass("ac_hidden");
+
+  e.preventDefault();
+});
+});
