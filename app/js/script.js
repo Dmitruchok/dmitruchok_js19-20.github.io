ce37e83dbade888bd6bc2efc409a2e57baf206da
Andrei Dmytruk <mallboroks@mail.ru>
2016-11-11 12:59:15 +0200

    20

--- app/js/script.js
+++ app/js/script.js
@@ -6,10 +6,10 @@ $(document).ready(function(){
 
     $(".accordion h5").click(function(){
 
-  $(this).next("p").slideToggle("slow")
-  .siblings("p:visible").slideUp("slow");
-  $(this).toggleClass("active");
-  $(this).siblings("h5").removeClass("active");
+      $(this).next("p").slideToggle("slow")
+      .siblings("p:visible").slideUp("slow");
+      $(this).toggleClass("active");
+      $(this).siblings("h5").removeClass("active");
   });
 
  });
